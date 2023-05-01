59ae25dd416ff433301bb8c10da95a3c48af5eeb
Benefrancis <benefrancis@gmail.com>
2023-05-01 19:17:28 -0300

    Criamos uma instância do EntityManager para gerenciamentos das transações com o banco de dados.

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -7,7 +7,8 @@ import jakarta.persistence.Persistence;
 public class Main {
     public static void main(String[] args) {
 
-
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
+        EntityManager manager = factory.createEntityManager();
 
     }
 }
